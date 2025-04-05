028bc8a2e9ef25e31e988909747068c21858cca2
Alan Critelli <alancritelli@gmail.com>
2025-04-05 10:45:09 -0300

    init: create base project files and folders

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/projects', (req, res) => {
+  res.json(['Projeto 1', 'Projeto 2'])
+})
+
+app.post('/projects', (req, res) => {
+  res.json(['Projeto 1', 'Projeto 2', 'Projeto 3'])
+})
+
+app.put('/projects/:id', (req, res) => {
+  res.json(['Projeto 4', 'Projeto 2', 'Projeto 3'])
+})
+
+app.delete('/projects/:id', (req, res) => {
+  res.json(['Projeto 2', 'Projeto 3'])
+})
+
+app.listen(3333, () => {
+  console.log('Backend iniciado 🚀')
+})
